8a77c0f165f1d921bd12a7e5f5630566efeff907
VonC <vonc@laposte.net>
2024-12-31 22:47:19 +0100

    feat(bat): build.bat calls update-version, detect failed release and cancel it

--- build.bat
+++ build.bat
@@ -16,6 +16,8 @@
 ::    b rel
 ::    brel "rel_title=Initial project template"
 ::    brel a "e f" "rel_title=Initial project template" "g t h"
+::    b a rel "e f" "rel_title=Initial project template" "g t h"
+::    b a rel "e f" prj_error "rel_title=Initial project template" "g t h"
 ::
 :: Return Value: 0 - Success, 1 - Error
 ::********************************************************************
@@ -25,15 +27,7 @@
 ::  ===============================================
 for %%i in ("%~dp0") do SET "build_dir=%%~fi"
 
-if defined called_from_update-version ( set "QUIET_PRJ=true" )
 call <NUL "%build_dir%\senv.bat"
-set "QUIET_PRJ="
-
-set "build_msg="
-if defined called_from_update (
-  set "build_msg= (from update-version)"
-  goto:set_param_echos
-)
 
 ::  ===============================================
 ::  PARSE PARAMETERS
@@ -68,14 +62,12 @@ if "%~1"=="rel" (
 shift
 goto loop
 :end
-endlocal & set "build_params=%build_params%" & set "build_params-uv=%build_params-uv%" & set "PRJ_REL_TITLE=%PRJ_REL_TITLE%"
+endlocal & set "build_params=%build_params%" & set "build_params-uv=%build_params-uv%" & set "PRJ_REL_TITLE=%PRJ_REL_TITLE%" & set "build_must_fail=%build_must_fail%"
 
-:set_param_echos
 set "build_params_echos="
 if not defined build_params ( goto:build_params-uv_echos )
 setlocal enabledelayedexpansion
   set "build_params_echos=%build_params:"=‟%"
-  echo build_params_echos='!build_params_echos!'
 endlocal & set "build_params_echos=%build_params_echos%"
 
 :build_params-uv_echos
@@ -86,7 +78,7 @@ if not defined build_params-uv ( goto:info_params )
 endlocal & set "build_params-uv_echos=%build_params-uv_echos%"
 
 :info_params
-%_info% "build_params for build%build_msg%: '%build_params_echos%'"
+%_info% "build_params for build: '%build_params_echos%'"
 %_info% "build_params for update-version (rel for 'make release'): '%build_params-uv_echos%'"
 if defined PRJ_REL_TITLE (
   %_info% "Release title PRJ_REL_TITLE: '%PRJ_REL_TITLE%'"
@@ -95,44 +87,23 @@ if defined PRJ_REL_TITLE (
 ::  ===============================================
 ::  UPDATE VERSION
 ::  ===============================================
-set "failed_update-version="
-if not defined called_from_update-version (
-  set "has_been_called_from_update-version="
-  set "called_from_build=true"
-  call "%build_dir%\tools\update-version.bat" %build_params-uv%
-  if errorlevel 1 (
-    set "failed_update-version=true"
-  )
-  set "QUIET_PRJ=true"
-  call <NUL "%build_dir%\senv.bat"
-  set "QUIET_PRJ="
-) else (
-  %_info% "Build Called from update-version"
-  set "build_msg= (from update-version)"
-)
-set "called_from_update-version="
-
-if defined has_been_called_from_update-version (
-  %_ok% "Build already called from update-version, no need to build twice"
-  set "has_been_called_from_update-version="
-  call:build_unset
-  goto:eof
-)
-
-if defined failed_update-version (
+call "%build_dir%\tools\update-version.bat" %build_params-uv%
+if errorlevel 1 (
   call:build_unset
   call "%~dp0tools\batcolors\echos.bat" :fatal "update-version FAILED, code '%ERRORLEVEL%'" 3
   goto:eof
 )
+set "QUIET_PRJ=true"
+call <NUL "%build_dir%\senv.bat"
+set "QUIET_PRJ="
 
 ::  ===============================================
 ::  BUILD PROJECT
 ::  ===============================================
 call "%project_dir%\tools\get-version.bat"
 %_info% "----------------------------------------"
-%_info% "Build the project '%project_dir_name%'%build_msg%, version '%project_version%'"
+%_info% "Build the project '%project_dir_name%', version '%project_version%'"
 %_info% "----------------------------------------"
-set "build_msg="
 
 mkdir "%project_dir%\target" 2>NUL
 del /F /Q "%project_dir%\target\*.*" 2>NUL
@@ -145,10 +116,47 @@ if not errorlevel 1 (
   %_ok% "project '%project_dir_name%' build successful"
   call:build_unset
 ) else (
+  %_error% "project '%project_dir_name%' build FAILED for version '%project_version%'"
+  call:has_a_release_just_been_made
+  if defined a_release_has_just_been_made (
+    set "a_release_has_just_been_made="
+    call:reset_pre_release
+  )
   call:build_unset
   call "%~dp0tools\batcolors\echos.bat" :fatal "project '%project_dir_name%' build FAILED, code '%ERRORLEVEL%'" 3
 )
 goto:eof
+
+::##################################################
+::  CHECK IF A RELEASE HAS JUST BEEN MADE
+::##################################################
+:has_a_release_just_been_made
+set "a_release_has_just_been_made="
+
+for /f "delims=" %%i in ('git tag --points-at HEAD') do (
+    if "%%i"=="v%project_version%" (
+        set "a_release_has_just_been_made=true"
+        %_info% "[%~nx0] A release has just been made"
+        goto:eof
+    )
+)
+%_info% "[%~nx0] No release has been made"
+goto:eof
+
+
+::##################################################
+::  RESET PRE-RELEASE BECAUSE BUILD FAILED
+::##################################################
+:reset_pre_release
+%_task% "[%~nx0] Must reset pre-release state (build failed): git reset, git tag -d 'v%project_version%'"
+git -C "%project_dir%" reset @~1
+if errorlevel 1 ( %_fatal% "[%~nx0] Unable to reset hard to previous commit of '%project_dir%'" 311 )
+%_ok% "[%~nx0] Git repository reset to previous commit"
+git -C "%project_dir%" tag -d "v%project_version%"
+if errorlevel 1 ( %_fatal% "[%~nx0] Unable to delete git tag 'v%project_version%' of '%project_dir%'" 312 )
+%_ok% "[%~nx0] Git tag 'v%project_version%' deleted"
+goto:eof
+
 
 ::##################################################
 ::  CLEANUP
@@ -163,4 +171,8 @@ call "%build_dir%\senv.bat" unset
 set "build_dir="
 set "PRJ_REL_TITLE="
 set "build_must_fail="
+set "called_from_build="
+set "build_params_echos="
+set "build_params-uv_echos="
+set "params-uv="
 goto:eof
