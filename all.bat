5711cb6b950e85dac53c98bdec1119aad1558070
VonC <vonc@laposte.net>
2025-01-10 12:21:18 +0100

    fix(bat): all.bat restore all_dir variable unset by build or run

--- all.bat
+++ all.bat
@@ -12,6 +12,8 @@ if errorlevel 1 (
     call:unset_all
     exit /b 1
 )
+
+for %%i in ("%~dp0") do SET "all_dir=%%~fi"
 call "%all_dir%\senv.bat"
 %_task% "Start project '%project_dir_name%' run"
 call <NUL "%all_dir%\run.bat" %*
@@ -23,6 +25,7 @@ call:unset_all
 goto:eof
 
 :unset_all
+for %%i in ("%~dp0") do SET "all_dir=%%~fi"
 call "%all_dir%\senv.bat" unset
 set "all_dir="
 set "barg="
