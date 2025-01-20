47ae848e1a3dd65583536c05f3de7f919aa19377
VonC <vonc@laposte.net>
2025-01-20 21:38:48 +0100

    feat(tools): update-tag-message updated tag annotation message

---
+++ tools/git/update-tag-message.bat
@@ -0,0 +1,12 @@
+@echo off
+
+setlocal enabledelayedexpansion
+
+for %%i in ("%~dp0") do SET "run_dir=%%~fi"
+
+set "QUIET_PRJ=true"
+call <NUL "%run_dir%\..\..\senv.bat"
+
+cd "%run_dir%"
+if errorlevel 1 ( %_fatal% "unable to cd to '%run_dir%'" 1 )
+bash -c "./update-tag-message.sh %*"
