23c29ca49f0c5c5af9e0e4d15543415b136ada51
VonC <vonc@laposte.net>
2025-01-25 23:51:53 +0100

    feat(setup): add env and service name retrieval scripts
    This commit introduces two new shell scripts to dynamically determine environment
    and service names based on user and context.
    Used by `.env` for PS1 prompt.
    
    - Added `get_env_name.sh`: Determines the environment name (DEV, QAL, PPD, PRD,
    or UNK) based on the last character of the username.
    - Added `get_service_name.sh`: Determines the service name based on the current
    working directory against the permitted tools and the first three letters of
    the username.

---
+++ src/setups/env/bin/get_env_name.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+env_name() {
+  local envn
+  envn="${USER: -1}"
+
+  case "${envn}" in
+  '2' | 'c')
+    echo "DEV"
+  ;;
+  '1')
+    echo "QAL"
+  ;;
+  '6')
+    echo "PPD"
+  ;;
+  '0')
+    echo "PRD"
+  ;;
+  *)
+    echo "UNK"
+  ;;
+  esac
+}
+
+export -f env_name
---
+++ src/setups/env/bin/get_service_name.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Define the function to determine the service based on the current working directory
+service_name() {
+  local tools
+  tools="${1}"
+  local cwd
+  cwd=$(pwd)
+  local service
+
+  service=$(printf "%s" "${USER}"|head -c 3)
+  service="${service^^}"
+  if [[ "${service}" == "VON" ]]; then
+    service="CPLX"
+    else
+    service="${service}-CPLX"
+  fi
+  
+  # Split the tools string into an array using comma as the delimiter
+  IFS=',' read -ra tool_array <<< "${tools}"
+
+  # Loop over each tool in the array
+  for tool in "${tool_array[@]}"; do
+    # Trim any leading/trailing whitespace
+    tool=$(echo "$tool" | xargs)
+
+    # Check if the current working directory matches the tool
+    if [[ "$cwd" == */$tool/* || "$cwd" == */$tool ]]; then
+      # Convert the tool name to uppercase and append it to the service
+      echo "${service}-$(echo "$tool" | tr '[:lower:]' '[:upper:]')"
+      return
+    fi
+  done
+
+  # If no tools match, return the default service name
+  echo "${service}"
+}
+
+export -f service_name
