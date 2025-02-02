ebd3e81255a1b8900915c8a6ffeab501bd264813
VonC <vonc@laposte.net>
2025-02-02 01:09:09 +0100

    feat(install): Improve Windows installer
    The Windows installer script now uses environment variables to tool and version
    to install. It also determine installation paths from the setups.properties file.
    
    What:
    - Added logic to determine installation and source directories within the script.
    - Introduced environment variables for tool and version selection.
    - Enabled the script to retrieve the installation path from a properties file.
    - Updated the SCP and SSH commands to use paths determined at runtime.
    - Improved error handling and logging.
    - Added a warning message instead of a fatal error when the log file cannot be opened.
    - Displayed the last 10 lines of the temporary file, before exiting, to provide more context about the failure.

--- src/install/install.bat
+++ src/install/install.bat
@@ -14,22 +14,54 @@ if "%SSH_CONFIG_ENTRY%" == "" (
     %_fatal% "SSH_CONFIG_ENTRY is not defined (must be an SSH alias as alias to remote Linux server where a program is compiled)" 1
 )
 
-if "%CPLX_TOOL%" == "" (
-    %_fatal% "CPLX_TOOL is not defined (must be one of )" 1
-)
-for %%i in ("%~dp0") do SET "script_dir=%%~fi"
+for %%i in ("%~dp0") do SET "install_dir=%%~fi"
+set "install_dir=%install_dir:~0,-1%"
+for %%i in ("%~dp0..") do SET "src_dir=%%~fi"
 
-scp -r "%script_dir%/install/env/." %SSH_CONFIG_ENTRY%:%project_path%/tools/
-ssh %SSH_CONFIG_ENTRY% "cd %project_path%/tools && chmod 755 ./install && bash %project_path%/tools/install; echo $?" | tee "%install_dir%\temp.txt"
-FOR /F "delims=" %%i IN (temp.txt) DO SET "lastLine=%%i"
+for /f "tokens=* delims=" %%a in ('cygpath -u "%install_dir%"') do (
+    SET "install_dir_unix=%%~a"
+)
+for /f "tokens=* delims=" %%a in ('cygpath -u "%src_dir%"') do (
+    SET "src_dir_unix=%%~a"
+)
+
+for /f "tokens=* delims=" %%a in ('bash -c "source "${src_dir_unix}/utils/properties.sh"; properties_file="${src_dir_unix}/setups/setup.properties"; export properties_file; get_property services; export services; printf "%%s" $services"') do (
+    SET "cplx_services=%%~a"
+)
+if not defined cplx_services (
+    %_fatal% "cplx_services is not defined in '%src_dir_unix%/utils/properties'" 1
+)
+
+if "%CPLX_TOOL%" == "" ( set "CPLX_TOOL=%~1" )
+if "%CPLX_TOOL%" == "" (
+    %_fatal% "CPLX_TOOL is not defined (must be one of '%cplx_services%')" 2
+)
+if "%CPLX_VERSION%" == "" ( set "CPLX_VERSION=%~2" )
+if "%CPLX_VERSION%" == "" (
+    %_fatal% "CPLX_VERSION is not defined (must be one of '%cplx_services%')" 3
+)
+
+rem echo "install_dir_unix='%install_dir_unix%', src_dir_unix='%src_dir_unix%'"
+for /f "tokens=* delims=" %%a in ('bash -c "source "${src_dir_unix}/utils/properties.sh"; properties_file="${src_dir_unix}/setups/setup.properties"; export properties_file; get_property cplx_path; export cplx_path; printf "%%s" $cplx_path"') do (
+    SET "cplx_path=%%~a"
+)
+if not defined cplx_path (
+    %_fatal% "cplx_path is not defined in '%src_dir_unix%/utils/properties'" 4
+)
+
+scp -r "%install_dir_unix%/env/." %SSH_CONFIG_ENTRY%:%cplx_path%/tools/
+ssh %SSH_CONFIG_ENTRY% "cd %cplx_path%/tools && chmod 755 ./install && bash ./install %CPLX_TOOL% %CPLX_VERSION%; echo $?" | tee "%install_dir%\temp.txt"
+FOR /F "delims=" %%i IN ("%install_dir%\temp.txt") DO SET "lastLine=%%i"
 %_info% "vvvvvvvvvvvvvvvvvvvvvvvvvvv"
 %_info% "Exit status: %lastLine%"
 %_info% "^^^^^^^^^^^^^^^^^^^^^^^^^^^"
-scp "%SSH_CONFIG_ENTRY%:%project_path%/log" log
-start /b "VSCode" "%PRGS%\vscodes\current\bin\code.cmd" log
-if not %lastLine%==0 (
-    %_fatal% "Installation failed" 2
+scp "%SSH_CONFIG_ENTRY%:%cplx_path%/tools/python/log" "%install_dir_unix%/log"
+if not errorlevel 1 (
+    start /b "VSCode" "%PRGS%\vscodes\current\bin\code.cmd" "%install_dir%\log"
+) else (
+    %_warning% "Failed to open log file at '%install_dir%'"
 )
+if not %lastLine%==0 ( tail -10 "%install_dir%\temp.txt" && %_fatal% "Installation failed" 5 )
 %_ok% "Installation executed"
 
 goto:eof
