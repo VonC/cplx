a634e59703d7675f0ea03f15b42cae57a858e99c
VonC <vonc@laposte.net>
2025-02-16 20:49:16 +0100

    feat(setup): Create symbolic link for python.1 man page
    Why: The python.1 man page was missing, causing errors when using the `man`
         command with python.
    	 This patch ensures the man page is available by creating a symbolic link.
    
    What:
    - Created a symbolic link from python.1 to python3.1 in `/current/share/man/man1/`.
    - The symbolic link is only created if python.1 doesn't already exist.

--- src/install/env/python/bin/manw.sh
+++ src/install/env/python/bin/manw.sh
@@ -5,5 +5,9 @@ source "${DIR}/echos"
 # shellcheck source=/dev/null
 source "${DIR}/setenv"
 
+if [[ ! -e "${DIR}/current/share/man/man1/python.1" ]]; then
+  ln -nfs "$(readlink "${DIR}/current/share/man/man1/python3.1")" "${DIR}/current/share/man/man1/python.1"
+fi
+
 command man "$@"
 exit $?
