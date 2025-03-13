1a21b7609c3f71f292a4375da0daca7cdafc68b8
VonC <vonc@laposte.net>
2025-03-13 21:35:49 +0100

    fix(install): do not impose `-lssl`/`-lcrypto`
    Why: In tools/install_functions.sh#setenv(), LDFLAGS included for all projects
         those two flags.
    	 But `-lssl -lcrypto` said should be added to `LDFLAGS` on a
    	 project-by-project basis.
    	 Typically, the openssl projects (ssl111 or ssl3) must not have those flags,
    	 or you get the error:
    	 ```
    	 /usr/bin/ld: cannot find -lssl
    	 /usr/bin/ld: cannot find -lcrypto
    	```
    
    What:
    - Removed the `-lssl -lcrypto` flags from the linker options

--- src/install/env/install_functions.sh
+++ src/install/env/install_functions.sh
@@ -74,8 +74,8 @@ function setenv() {
                     -Wl,-rpath=${LD_LIBRARY_PATH} \
                     -Wl,--dynamic-linker=${dynamic_linker} \
                     -Wl,--export-dynamic \
-                    -L${root}/usr/lib64 -L${root}/usr/lib -L${root}/lib64 -L${root}/lib \
-                    -lssl -lcrypto"
+                    -L${root}/usr/lib64 -L${root}/usr/lib -L${root}/lib64 -L${root}/lib"
+                    # -lssl -lcrypto"
                     # -Wl,-verbose"
                     # Standard lib directories.
                     # No -nodefaultlibs needed with a proper sysroot.
