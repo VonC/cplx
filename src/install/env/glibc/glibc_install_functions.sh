2d005c82dcd889610d47b26b618c331562e75cad
VonC <vonc@laposte.net>
2025-04-01 10:25:06 +0200

    fix(glibc): Correct glibc build process
    Why:
    The glibc build process was incorrect and failed.
    It tried to build within the source directory instead of using a dedicated build
    directory, which is mandatory for glibc (out-of-tree build).
    It also assumed hardcoded path to `gcc`, which is not suitable for
    cross-compilation.
    Moreover, the build directory was not created in predictable location relative
    to the source.
    
    What:
    - Added `glibc_setenv` to set `COMPILER_PATH` and augment `PATH`
    - Set object directory location predictably relative to source to
      `${tool_src}/../build`
    - Modified `configure` and `build` functions to build in the object directory
    - Removed the faulty `sed` command in `configure` aimed to update `configure`
    - Replaced hardcoded checks for gcc with dynamic approach
    - Updated the Makefile to reflect the changes and avoid make errors
    - Added checks for `CPLX_CHECK_SRC` variable existence and made build
      conditional to presence of `CPLX_CHECK_SRC`
    - The execution location for make changed from source directory to the build
      directory.
    - Updated log and error messages for build failures.

--- src/install/env/glibc/glibc_install_functions.sh
+++ src/install/env/glibc/glibc_install_functions.sh
@@ -1,14 +1,39 @@
 #!/bin/bash
 
+function glibc_setenv() {
+    # shellcheck disable=SC1091
+    # shellcheck disable=SC2154
+    source "${tools}/update_path_variables.sh"
+    if ! declare -f update_path_variable >/dev/null 2>&1; then
+        fatal "No path var function present" 117
+    fi
+    # shellcheck disable=SC2154
+    if [[ ! -e "${tools}/gcc/bin/gcc" ]]; then
+        fatal "No gcc present at" 118
+    fi
+    # shellcheck disable=SC2154
+    update_path_variable "PATH" "${tools}/gcc/bin"
+    info "glibc: PATH update with gcc/bin: '${PATH}'"
+    export COMPILER_PATH="${tools}/gcc/bin"
+    info "glibc: COMPILER_PATH update with gcc/bin: '${COMPILER_PATH}'"
+}
+
 function configure() {
     task "config.log not present: reconfigure"
 
-    if [[ ! -e configure ]]; then
-        task "Must make configure in '$(pwd)'"
+    # shellcheck disable=SC2154
+    objdir=$(readlink -f "${tool_src}/../build")
+
+    mkdir -p "${objdir}" || fatal "Unable to create build directory '${objdir}'" 17
+    cd "${objdir}" || fatal "Unable to change to build directory '${objdir}'" 18
+
+    # shellcheck disable=SC2154
+    if [[ ! -e "${tool_src}/configure" ]]; then
+        task "Must make configure from '$(pwd)' for sources in '${tool_src}'"
         make configure || fatal "Unable to make configure in '$(pwd)'" 15
-        ok "'configure' is now created in '$(pwd)'"
+        ok "'configure' is now created in '${tool_src}'"
     else
-        ok "'configure' is present in '$(pwd)'"
+        ok "'configure' is present in '${tool_src}'"
     fi
     
     # Build the configure command as an array
@@ -17,30 +42,38 @@ function configure() {
         "--prefix=${tool_prefix}" \
     )
 
-    sed -i "s,ssldir/lib\",ssldir/lib64\",g" configure || fatal "Unable to update 'configure' ssldir/lib to ssldir/lib64 in '$(pwd)'" 16
+    # sed -i "s,ssldir/lib\",ssldir/lib64\",g" configure || fatal "Unable to update 'configure' ssldir/lib to ssldir/lib64 in '$(pwd)'" 16
 
     # Display the command with its parameters.
-    info "Running configure command: ${configure_cmd[*]}"
+    task "Must run configure command: ${configure_cmd[*]} from pwd '$(pwd)', should be objdir '${objdir}'"
+    info "PATH='${PATH}'"
+    info "which gcc=$(which gcc), gcc version: $(gcc --version)"
 
-    mkdir -p "${tool_src}/build" || fatal "Unable to create build directory in '$(pwd)'" 17
-    cd "${tool_src}/build" || fatal "Unable to change to build directory in '$(pwd)'" 18
     rm -f "${tool_src}/config.log"
-    ln -nfs build/config.log "${tool_src}/config.log"
-
+    ln "${objdir}/config.log" -nfs "${tool_src}/config.log"
     # Execute the configure command.
     if ! "${configure_cmd[@]}"; then
         fatal "configure ERROR" 199
     fi
+    echo "creating Makefile" >> "${objdir}/config.log"
     ok "configure done"
 }
 
 function build() {
-    if [[ ! -e git-add && ! -e ${CPLX_TOOL} ]]; then
-        task "Must make in '$(pwd)'"
+
+    objdir=$(readlink -f "${tool_src}/../build")
+    cd "${objdir}" || fatal "Unable to change to build directory '${objdir}'" 180
+
+    get_property CPLX_CHECK_SRC
+    if [[ -z ${CPLX_CHECK_SRC} ]]; then
+        fatal "CPLX_CHECK_SRC is not set" 20
+    fi
+    if [[ ! -e "${CPLX_CHECK_SRC}" ]]; then
+        task "Must make in '$(pwd)' (CPLX_CHECK_SRC='${CPLX_CHECK_SRC}' is missing in '$(pwd)')"
         make || fatal "Unable to make in '$(pwd)'" 19
         ok "make is now done in '$(pwd)'"
     else
-        ok "${CPLX_TOOL} already compiled in '$(pwd)'"
+        ok "${CPLX_TOOL} already compiled in '$(pwd)' (CPLX_CHECK_SRC='${CPLX_CHECK_SRC}' is there)"
     fi
 }
 
