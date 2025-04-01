10feea240cb535fcb57303b8bcb5d547d179588f
VonC <vonc@laposte.net>
2025-04-01 15:48:27 +0200

    fix(install): Resolve bootstrap and automake issues
    Why:
    The previous Make4 installation script had issues bootstrapping and setting up
    the necessary environment because of missing dependencies.
    Also, autopoint was not correctly patched, leading to inconsistencies.
    The build process also lacked a reliable check for build success/failure,
    sometimes reporting success even when the target executable was not built.
    
    What:
    - Added `make4_setenv` function to properly set environment variables, including
      updating PATH and PERLLIB for automake116.
    - Patched `autopoint` to use the correct paths within the tools directory.
    - Updated configure command to use the project's bootstrap script and to
      disable Guile support (`--with-guile=no`).
    - Modified the `build` function to rely on the `CPLX_CHECK_SRC` variable to
      accurately assess the build outcome looking for the compiled result.
    - Added missing dependencies to the package list for RHEL 7.9:
      `perl-libintl`.  Removed `automake` as we provide `automake116`.

--- src/install/env/make4/make4_install_functions.sh
+++ src/install/env/make4/make4_install_functions.sh
@@ -1,11 +1,43 @@
 #!/bin/bash
 
+function make4_setenv() {
+    # shellcheck disable=SC1091
+    # shellcheck disable=SC2154
+    source "${tools}/update_path_variables.sh"
+    if ! declare -f update_path_variable >/dev/null 2>&1; then
+        fatal "No path var function present" 117
+    fi
+    # shellcheck disable=SC2154
+    if [[ ! -e "${tools}/automake116/current/bin/automake" ]]; then
+        fatal "No automake116 present at" 118
+    fi
+    # shellcheck disable=SC2154
+    update_path_variable "PATH" "${tools}/automake116/current/bin"
+    info "make4: PATH update with automake116/current/bin: '${PATH}'"
+    # shellcheck disable=SC2154
+    update_path_variable "PERLLIB" "${tools}/automake116/current/share/automake-1.16"
+    info "make4: PATH update with automake116/current/share/automake-1.16: '${PERLLIB}'"
+
+    # shellcheck disable=SC2154
+    if ! grep "${tools}" "${root}/usr/bin/autopoint" >/dev/null 2>&1; then
+        task "make4_setenv: must patch autopoint /usr"
+        if ! sed -i "s,/usr,${tools}/tool/root/usr,g" "${root}/usr/bin/autopoint"; then
+            fatal "make4_setenv: unable to patch autopoint" 119
+        else
+            ok "make4_setenv: autopoint patched"
+        fi
+    else
+        ok "make4_setenv: autopoint already patched"
+    fi
+}
+
 function configure() {
     task "config.log not present: reconfigure"
 
     if [[ ! -e configure ]]; then
-        task "Must make configure in '$(pwd)'"
-        make configure || fatal "Unable to make configure in '$(pwd)'" 15
+        task "Must make configure in '$(pwd)': bootstrap"
+        # shellcheck disable=SC2154
+        "${tool_src}/bootstrap" || fatal "Unable to bootstrap in '$(pwd)'" 15
         ok "'configure' is now created in '$(pwd)'"
     else
         ok "'configure' is present in '$(pwd)'"
@@ -15,6 +47,7 @@ function configure() {
     local configure_cmd=( 
         "${tool_src}/configure" \
         "--prefix=${tool_prefix}" \
+        "--with-guile=no" \
     )
 
     sed -i "s,ssldir/lib\",ssldir/lib64\",g" configure || fatal "Unable to update 'configure' ssldir/lib to ssldir/lib64 in '$(pwd)'" 16
@@ -30,12 +63,16 @@ function configure() {
 }
 
 function build() {
-    if [[ ! -e git-add && ! -e ${CPLX_TOOL} ]]; then
-        task "Must make in '$(pwd)'"
+    get_property CPLX_CHECK_SRC
+    if [[ -z ${CPLX_CHECK_SRC} ]]; then
+        fatal "CPLX_CHECK_SRC is not set" 20
+    fi
+    if [[ ! -e "${CPLX_CHECK_SRC}" ]]; then
+        task "Must make in '$(pwd)' (CPLX_CHECK_SRC='${CPLX_CHECK_SRC}' is missing in '$(pwd)')"
         make || fatal "Unable to make in '$(pwd)'" 19
         ok "make is now done in '$(pwd)'"
     else
-        ok "${CPLX_TOOL} already compiled in '$(pwd)'"
+        ok "${CPLX_TOOL} already compiled in '$(pwd)' (CPLX_CHECK_SRC='${CPLX_CHECK_SRC}' is there)"
     fi
 }
 
